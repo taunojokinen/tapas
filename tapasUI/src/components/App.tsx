68ef20c1f4a66df79a784c32f31f674c75d17407
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2025-03-05 15:05:24 +0200

    logo lisätty

--- tapasUI/src/components/App.tsx
+++ tapasUI/src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../pictures/logo.png';
 
 const App: React.FC = () => {
     return (
         <div>
             <h1>Welcome to My React App</h1>
+            <img src={logo} alt="Logo" />
             {/* Additional components and application logic can be added here */}
         </div>
     );
