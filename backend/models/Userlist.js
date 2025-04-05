38ad15892499b29b2848278b9dfe4c64800ade9f
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2025-04-05 16:37:38 +0300

    Userslistan lukeminen .csv tiedostosta lisätty

---
+++ backend/models/Userlist.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    supervisor: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ['admin', 'director', 'supervisor', 'employer'],  // 🔹 Roolit: admin, director, supervisor, employer
+        default: 'employer',
+    },
+});
+
+const Userlist = mongoose.model("Userlist", UserSchema);
+
+module.exports = Userlist;
