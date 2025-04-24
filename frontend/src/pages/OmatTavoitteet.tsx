5ba9a0435b0ea812f94eaf4a43594e1817e34ee3
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2025-04-24 14:15:49 +0300

    käyttäjänimi lisättu omiin tavoitteisiin

--- frontend/src/pages/OmatTavoitteet.tsx
+++ frontend/src/pages/OmatTavoitteet.tsx
@@ -3,22 +3,21 @@ import MyMission from "../components/omatTavoitteet/mymission"; // Adjust the pa
 import KeyObjectives from "../components/omatTavoitteet/keyObjectives"; // Adjust the path if necessary
 import MyTasks from "../components/omatTavoitteet/myTasks"; // Adjust the path if necessary
 import MyCurrentState from "../components/omatTavoitteet/myCurrenState"; // Adjust the path if necessary
+import useAuth from "../hooks/useAuth"; // Import the custom hook
 
 
 const OmatTavoitteet = () => {
-
+  const { username } = useAuth();
 
   return (
     <div>
-      <h2 className="text-xl font-bold text-gray-800">Omat tavoitteet</h2>
+      <h2 className="text-xl font-bold text-gray-800">Käyttäjä {username} Omat tavoitteet</h2>
       <div className="flex flex-col space-y-6">
         <div className="bg-white p-4 rounded-lg shadow">
           <MyMission />
           <KeyObjectives />
           <MyTasks />
           <MyCurrentState />
-
-
         </div>
       </div>
     </div>
