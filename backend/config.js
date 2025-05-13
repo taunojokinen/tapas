c2f1a73958c9a3a6f197f58d1fbff45263599c83
Mirja Pirinen <mirja.pirinen@hotmail.com>
2025-05-13 09:05:57 +0300

    Korjaus uloskirjautumiseen

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     JWT_SECRET: process.env.JWT_SECRET || 'defaultSecretKey', // Fallback to default if not in .env
-    JWT_EXPIRES_IN: '12h', // Token expiration time
+    JWT_EXPIRES_IN: '1h', // Token expiration time
     DB_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/defaultDatabase',
     PORT: process.env.PORT || 3000,
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/defaultDatabase', // MongoDB connection string
